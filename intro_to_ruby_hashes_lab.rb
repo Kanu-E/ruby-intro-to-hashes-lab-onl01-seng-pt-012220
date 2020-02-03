703d9707a82226b772a73aa3ac6e5d6c97a8ff42
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 21:18:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,8 +19,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => {pieces: 4}},
-	           {:names => {}}
+	monopoly = {:railroads => {:names => {}}},
+	          
 	           
 
 end
