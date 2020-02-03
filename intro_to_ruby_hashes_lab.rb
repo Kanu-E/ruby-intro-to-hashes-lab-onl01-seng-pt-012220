754c97e3a7f9d5fdff03f4df0d9324bfd36a03f1
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 21:19:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => {:names => {""}}},
+	monopoly = {:railroads => 
+	            {:names => {""}}},
 	          
 	           
 
